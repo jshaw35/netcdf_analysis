e307a26a1b1be7e2426c63482224ab6e08ebe5ed
Jonah Krammer Shaw <jonahks@storm.uio.no>
2019-11-27 13:18:26 +0100

    streamlined jupy_test and integrated with functions and imports

--- .ipynb_checkpoints/functions-checkpoint.py
+++ .ipynb_checkpoints/functions-checkpoint.py
@@ -1,9 +1,12 @@
+from imports import *
+
 import xarray as xr
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
-
+import matplotlib.path as mpath
+import cartopy.crs as ccrs
+import cartopy as cy
 
 # Functions from Abisko 2019 examples
 def masked_average(xa:xr.DataArray,
@@ -64,7 +67,132 @@ def __weighted_average_with_mask(dim, mask, weights, xa, xa_copy):
         xa_weighted_average = xa_copy.mean(dim)
     return xa_weighted_average
 
+def polarCentral_set_latlim(lat_lims, ax):
+    ax.set_extent([-180, 180, lat_lims[0], lat_lims[1]], ccrs.PlateCarree())
+    # Compute a circle in axes coordinates, which we can use as a boundary
+    # for the map. We can pan/zoom as much as we like - the boundary will be
+    # permanently circular.
+    theta = np.linspace(0, 2*np.pi, 100)
+    center, radius = [0.5, 0.5], 0.5
+    verts = np.vstack([np.sin(theta), np.cos(theta)]).T
+    circle = mpath.Path(verts * radius + center)
 
-# ## Application 1: Weigted global average:
-# Grid cells have different area, so when we do the global average, they have to be weigted by the area of each grid cell.
-# Here we do it for 2 m temperature:
+    ax.set_boundary(circle, transform=ax.transAxes)
+
+def add_map_features(ax):
+    '''
+    Single line command for xarray plots
+    '''
+    ax.coastlines()
+    gl = ax.gridlines()
+    ax.add_feature(cy.feature.BORDERS);
+    gl = ax.gridlines()#draw_labels=True)
+    gl.xlabels_top = False
+    gl.ylabels_right = False
+    
+    
+def interpretNS(stringin):
+    '''
+    Interprets the string name of CALIOP longitudinally 
+    averaged SLF by isotherm data and returns a weight 
+    for the latitude range
+    '''
+    lat_range = stringin[11:-4]
+    first = lat_range[:3]
+    second = lat_range[-3:]
+    
+    if first[-1] is 'N':
+        low = int(first[:2])
+    else: low = -1 * int(first[:2])
+    if second[-1] is 'N':
+        high = int(second[:2])
+    else: high = -1 * int(second[:2])
+    avg_lat = np.abs(np.mean([low,high]))
+    weight = np.cos(np.pi/180*avg_lat)
+    
+    return weight, min([low, high]), max([low,high])
+
+def sp_map(*nrs, projection = ccrs.PlateCarree(), **kwargs):
+    return plt.subplots(*nrs, subplot_kw={'projection':projection}, **kwargs)
+
+
+def select_loc_to_pandas(dataset, coords):
+    '''
+    This function takes an xarray dataset and a (lat, lon) coordinate iterable.
+    It selects the data for the location and returns a pandas datafram object.
+    '''
+    _xr_ds = xr.Dataset() # empty xarray Dataset
+    for vals in dataset:
+        _da = dataset[vals]        
+        _da = _da.sel(lat=coords[0], lon=coords[1], method='nearest')
+        _xr_ds[vals]=_da
+    _df = _xr_ds.to_dataframe()
+    return _df
+
+
+def process_caliop(files, obs_dir):
+    all_caliop = pd.DataFrame()
+    weights = 0; avg = np.zeros(5);
+    for file in files:
+        _path = obs_dir + file # Get full file path
+        _name = 'CALIOP_' + file[11:-4]   # Pick out latitude data from name
+        _weight, _, _ = interpretNS(file)
+        _slice = pd.read_table(_path, sep="\s+", names=['Isotherm', _name])
+        all_caliop = pd.concat([all_caliop, _slice[_name]], axis=1, sort=False)
+
+        # Do the averaging
+        avg += _weight * _slice[_name]
+        weights += _weight
+
+    # Add the Isotherm colum and set it as an index
+    all_caliop = pd.concat([all_caliop, _slice['Isotherm']], axis=1, sort=False)
+    all_caliop = all_caliop.set_index('Isotherm')
+    all_caliop['CALIOP Average'] = np.array(avg / weights)
+    
+    return all_caliop
+
+def plot_slf_isotherms(ds):
+    slf_isotm = ds['SLF_ISOTM_AVG']    
+    
+    fig1, axes1 = plt.subplots(nrows=3,ncols=3, subplot_kw={'projection': ccrs.PlateCarree()}, figsize=[20,10]);
+
+    cmin_p = np.nanmin(slf_isotm)
+    cmax_p = np.nanmax(slf_isotm)
+
+    cmap_p = 'bwr'
+    nlevels = 41
+    cmap2 = plt.get_cmap(cmap_p)
+
+    if cmin_p == cmax_p:
+       cmax_p = cmax_p + 0.00001
+
+    levels = np.linspace(cmin_p,cmax_p,nlevels)
+
+    for data, ax in zip(slf_isotm, axes1.flatten()):
+        iso = data['isotherms_mpc'].values - 273.15
+        map = data.plot(ax=ax, transform=ccrs.PlateCarree(), cmap='bwr', 
+                        robust=True, add_colorbar = False, levels=levels)
+
+        ax.set_title('SLF at %s' % str(iso), fontsize=18)
+        ax.coastlines()
+
+    cb_ax = fig1.add_axes([0.325, 0.05, 0.4, 0.04])
+    cbar = plt.colorbar(map, cax=cb_ax, extend='both', orientation='horizontal', fraction=0.046, pad=0.04)
+    cbar.ax.tick_params(labelsize=22)
+    cbar.ax.set_xlabel('Supercooled Liquid Fraction', fontsize=16)
+
+    fig1.suptitle('SLF distribution across isotherms', fontsize=28)
+
+    return fig1
+
+def add_weights(ds):
+    gw = ds['gw']    
+
+    _wgs = ds['TS'].copy().mean(dim = 'time', skipna=True)
+    _wgs = (_wgs * 0 + 1) * gw # copy gw into the 2d array
+    _wgs = _wgs / np.sum(_wgs)  # Normalize
+    _wgs.name = 'cell_weight'
+
+    ds['cell_weight'] = _wgs
+    
+    return ds
---
+++ imports.py
@@ -0,0 +1,65 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.2'
+#       jupytext_version: 1.2.4
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %%
+import os
+from datetime import datetime
+import sys
+
+import pandas as pd
+import xarray as xr
+
+import numpy as np
+from numpy.polynomial.polynomial import polyfit
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.path as mpath
+import matplotlib.gridspec as gridspec
+from matplotlib.colors import LogNorm
+
+import cartopy as crt
+import cartopy.crs as ccrs
+
+import seaborn as sns
+import importlib as il
+import glob
+import random
+#import pyaerocom as pya
+
+def load_and_reload():
+    '''
+    the code below automatically reload modules that
+    have being changed when you run any cell.
+    If you want to call in directly from a notebook you
+    can use:
+    Example
+    ---
+    >>> %load_ext autoreload
+    >>> %autoreload 2
+    '''
+    from IPython import get_ipython
+
+    try:
+        _ipython = get_ipython()
+        _ipython.magic('load_ext autoreload')
+        _ipython.magic('autoreload 2')
+    except:
+        # in case we are running a script
+        _ipython.magic('no autoreload')
+        pass
+
+
+load_and_reload()
+
